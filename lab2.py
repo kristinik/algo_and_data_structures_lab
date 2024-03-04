999b31fc0966249f72e146972f38c51de06e05ad
kristinik <150954793+kristinik@users.noreply.github.com>
2024-03-04 15:16:46 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -20,4 +20,22 @@ P = 10
 print(find_sum(arr, P))
 
 
+def find_sum1(arr, target):
+    arr.sort()
+    n = len(arr)
 
+    for i in range(n - 2):
+        for j in range(i + 1, n - 1):
+            left = j + 1
+            right = n - 1
+            while left <= right:
+                mid = (left + right) // 2
+                current_sum = arr[i] + arr[j] + arr[mid]
+                if current_sum == target:
+                    return True
+                elif current_sum < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+    return False
