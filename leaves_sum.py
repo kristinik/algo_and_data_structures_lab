27d0f774ffe20e1a16672ba5f3b15dc1cdafe9fc
kristinik <150954793+kristinik@users.noreply.github.com>
2024-03-11 23:38:47 +0200

    Add files via upload

---
+++ leaves_sum.py
@@ -0,0 +1,26 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def Sums(root):
+    def helper(node, is_left_child):
+        if not node:
+            return 0
+        if not node.left and not node.right and is_left_child:
+            return node.value
+        return helper(node.left, True) + helper(node.right, False)
+
+    return helper(root, False)
+
+
+root = BinaryTree(4)
+root.left = BinaryTree(11)
+root.right = BinaryTree(21)
+root.right.left = BinaryTree(17)
+root.right.right = BinaryTree(6)
+
+
+print(Sums(root))
